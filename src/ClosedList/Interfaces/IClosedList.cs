c63836f014298ee31616eddf308b2ffa2723be47
Budaeva Valeriia <valeria.budaeva@yandex.ru>
2023-04-04 12:48:11 +0300

    add interface IClosedList

---
+++ src/ClosedList/Interfaces/IClosedList.cs
@@ -0,0 +1,14 @@
+﻿namespace ClosedList.Interfaces;
+
+internal interface IClosedList<T> : IList<T>
+{
+    void MoveNext(int step = 1);
+    void MoveBack(int step = 1);
+
+    T Head { get; }
+    T Current { get; }
+    T Previous { get; }
+    T Next { get; }
+
+    event EventHandler<T> HeadReached;
+}
