6dc47678903e6f50df479d966dd1976b12522e0a
Budaeva Valeriia <valeria.budaeva@yandex.ru>
2023-04-06 20:10:17 +0300

    add demo

--- src/ClosedList/Program.cs
+++ src/ClosedList/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bogus;
+using ClosedList;
+
+var closedList = new LinkedClosedList<string>(GetNouns(10));
+closedList.HeadReached += (s, e) => Console.WriteLine($"Head reached! Head: {e}");
+while (!Console.KeyAvailable)
+{
+    await Task.Delay(1000);
+    Console.WriteLine($"Previous: {closedList.Previous}, Current: {closedList.Current}, Next: {closedList.Next}");
+    closedList.MoveBack();
+}
+
+static IEnumerable<string> GetNouns(int count)
+{
+    var faker = new Faker();
+    return Enumerable.Range(1, count)
+        .Select(i => faker.Hacker.Noun());
+}
