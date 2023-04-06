52e8e41c4d7ad80c5dd4fe7e3c0952e8b9cc0d14
Budaeva Valeriia <valeria.budaeva@yandex.ru>
2023-04-06 19:59:22 +0300

    add ? suffix to declare a nullable reference type

--- src/ClosedList/Interfaces/IClosedList.cs
+++ src/ClosedList/Interfaces/IClosedList.cs
@@ -5,10 +5,10 @@ internal interface IClosedList<T> : IList<T>
     void MoveNext(int step = 1);
     void MoveBack(int step = 1);
 
-    T Head { get; }
-    T Current { get; }
-    T Previous { get; }
-    T Next { get; }
+    T? Head { get; }
+    T? Current { get; }
+    T? Previous { get; }
+    T? Next { get; }
 
-    event EventHandler<T> HeadReached;
+    event EventHandler<T?>? HeadReached;
 }
